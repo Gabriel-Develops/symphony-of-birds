a7c3508002b2ecc6fcf478de390307dcefec7c7e
Gabriel Rodriguez <garodri97@gmail.com>
2022-12-29 03:48:37 -0800

    added styling to bird element

--- src/components/Bird.jsx
+++ src/components/Bird.jsx
@@ -1,6 +1,6 @@
 const Bird = ({name, audioUrl, picUrl}) => {
     return  <div id="bird" style={{backgroundImage: `url(${picUrl})`}}>
-        <h3>{name}</h3>
+        <h2>{name}</h2>
         <audio autoPlay loop src={audioUrl}></audio>
     </div>
 }
