5f1c5c558a1a3081072ee755458cbf789b78510b
Gabriel Rodriguez <garodri97@gmail.com>
2022-12-29 16:24:15 -0800

    added node-fetch to be compatible with older node versions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
+const fetch = require('node-fetch')
 require('dotenv').config()
 
 app.use(cors())
